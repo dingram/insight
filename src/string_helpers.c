9982321e13b307f8d1491d287383e24e651887d7
Dave Ingram <dave@dmi.me.uk>
2008-07-16 14:47:12 +0000

    - Update hashing function to a faster one with (hopefully) fewer collisions

--- src/string_helpers.c
+++ src/string_helpers.c
@@ -28,6 +28,7 @@
  * Your fair use and other rights are in no way affected by the above.
  */
 
+#include <stdint.h>
 #include <insight.h>
 #if defined(_DEBUG_STRING) && !defined(_DEBUG)
 #define _DEBUG
@@ -219,7 +220,7 @@ char **strsplit(const char *input, const char sep, int *count) {
 	} while(0)
 
 
-unsigned long hash_path(const char *path) {
+unsigned long hash_path_old(const char *path) {
 
 	unsigned long k[] = { 0x9464a485, 0x542e1a94, 0x3e846bff, 0xb75bcfc3 };
 
@@ -301,4 +302,59 @@ unsigned long hash_path(const char *path) {
   ifree(p_orig);
 
 	return h0 ^ h1;
+}
+
+#undef get16bits
+#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
+  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#define get16bits(d) (*((const uint16_t *) (d)))
+#endif
+
+#if !defined (get16bits)
+#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
+                       +(uint32_t)(((const uint8_t *)(d))[0]) )
+#endif
+
+unsigned long hash_path(const char * path) {
+  /* bit of molding */
+  char *d_orig = strlast(path, '/');
+  char *data = d_orig + 1;
+  int len = strlen(data);
+
+  uint32_t hash = len, tmp;
+  int rem;
+  if (len <= 0 || data == NULL) return 0;
+  rem = len & 3;
+  len >>= 2;
+  for (;len > 0; len--) {
+    hash  += get16bits (data);
+    tmp    = (get16bits (data+2) << 11) ^ hash;
+    hash   = (hash << 16) ^ tmp;
+    data  += 2*sizeof (uint16_t);
+    hash  += hash >> 11;
+  }
+  switch (rem) {
+    case 3: hash += get16bits (data);
+            hash ^= hash << 16;
+            hash ^= data[sizeof (uint16_t)] << 18;
+            hash += hash >> 11;
+            break;
+    case 2: hash += get16bits (data);
+            hash ^= hash << 11;
+            hash += hash >> 17;
+            break;
+    case 1: hash += *data;
+            hash ^= hash << 10;
+            hash += hash >> 1;
+  }
+  hash ^= hash << 3;
+  hash += hash >> 5;
+  hash ^= hash << 4;
+  hash += hash >> 17;
+  hash ^= hash << 25;
+  hash += hash >> 6;
+
+  ifree(d_orig);
+
+  return hash;
 }
