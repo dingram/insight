98f851ab73449c754cb0cbe948824e243cfe89b6
Dave Ingram <dave@dmi.me.uk>
2008-09-01 08:13:47 +0000

    Added bplus test suite skeleton + makefile

---
+++ test/bplus/bplus-tests.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <debug.h>
+#include <bplus.h>
+
+void test_open_close() {
+}
+
+void test_format() {
+}
+
+void test_something() {
+}
+
+int main(int argc, char **argv) {
+  return 0;
+}
